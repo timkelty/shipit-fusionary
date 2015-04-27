ae07b3fb813cd115522efebdba09aa39286067dc
Tim Kelty <tim@fusionary.com>
2015-04-27 17:26:48 -0400

    Requiring shipit-plugins

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+  require('shipit-shared')(shipit);
+  require('shipit-db')(shipit);
+  require('shipit-assets')(shipit);
+  require('shipit-ssh')(shipit);
   require('./tasks/slack')(shipit);
   require('./tasks/app')(shipit);
 };
